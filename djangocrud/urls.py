ff675cdd6f00323b675bc92d27f9a4cb102e1115
JMDC <majin325yt@gmail.com>
2024-09-26 07:54:11 -0500

    modification for deployment on render

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -20,14 +20,14 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('tasks/', views.tasks, name='tasks' ),
-    path('tasks_completed', views.tasks_completed, name='tasks_completed'),
-    path('tasks/create/', views.create_tasks, name='create_task' ),
-    path('tasks/<int:task_id>/', views.task_detail, name='task_detail' ),
-    path('tasks/<int:task_id>/complete', views.completeTask, name='completeTask' ),
-    path('tasks/<int:task_id>/delete', views.deleteTask, name='deleteTask' ),
+    path('',views.home, name='home'),
+    path('signup/',views.signup,name='signup'),
+    path('tasks/',views.tasks,name='tasks'),
+    path('tasks_completed/',views.tasks_completed,name='tasks_completed'),
+    path('tasks/create/',views.create_task,name='create_task'),
+    path('tasks/<int:task_id>/',views.task_detail,name='task_detail'),
+    path('tasks/<int:task_id>/complete',views.complete_task,name='complete_task'),
+    path('tasks/<int:task_id>/delete',views.delete_task,name='delete_task'),
     path('logout/',views.signout,name='logout'),
-    path('signin/',views.sign_in,name='signin'), #la primera dirección es para la dirección de la url, la segunda para el módulo a usar
+    path('signin/',views.signin,name='signin'),
 ]
