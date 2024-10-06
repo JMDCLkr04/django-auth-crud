324e7797ce67b51ec36fad789b8d5e25a6832bef
JMDC <majin325yt@gmail.com>
2024-10-05 23:51:27 -0500

    upgrade for deployment on render

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+else:
+    ALLOWED_HOSTS.append('https://django-auth-crud-i2pl.onrender.com')
 
 # Application definition
 
