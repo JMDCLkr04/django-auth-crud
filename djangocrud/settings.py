f4b2c3b7d174f047c28812711da6aed5aa7d2ef4
JMDC <majin325yt@gmail.com>
2024-10-06 00:20:43 -0500

    upgrade on settings for deployment in render

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -26,14 +26,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
-
-ALLOWED_HOSTS = []
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-else:
-    ALLOWED_HOSTS.append('https://django-auth-crud-i2pl.onrender.com')
+#  'RENDER' not in os.environ
+DEBUG = True
+# ALLOWED_HOSTS = []
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# else:
+#     ALLOWED_HOSTS.append('https://django-auth-crud-i2pl.onrender.com')
 
 # Application definition
 
